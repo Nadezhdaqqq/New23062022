57be36e93fff710bd402c9285d0713eee8f7ffe3
NadezhdaM <nadia_07.87@inbox.ru>
2022-06-23 17:53:04 +0300

    про Машу

---
+++ exz_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("I love Маша");
+}
+else
+{
+    Console.Write("А где Маша, ");
+    Console.WriteLine(username);
+}
